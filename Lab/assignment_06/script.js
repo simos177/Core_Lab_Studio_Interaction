c728bd72c4c42ba983a0804ec34563fe1e27e4f9
Sam <simos177@newschool.edu>
2019-02-27 11:35:28 -0500

    update

---
+++ Lab/assignment_06/script.js
@@ -0,0 +1,36 @@
+var idName = "myElement";
+
+var myFirstElement = document.getElementById(idName);
+
+//myFirstElement.classList.add("newClass")
+//myFirstElement.classList.remove("newClass")
+myFirstElement.classList.toggle("newClass")
+
+var mySecondElement = document.getElementById(myList);
+
+var myNewNumber = 5;
+
+    myNewNumber = myNewNumber + 20;
+
+    mySecondElement.style.letterSpacing = myNewNumber + "px";
+
+
+var red = 255;
+var green = 0;
+var blue = 0;
+
+    mySecondElement.style.backgroundcolor = "rgb(" + red + "," + green + "," + blue + ")"
+
+
+/*********************/
+
+
+  if(true){
+
+      console.log("yes")
+
+
+  }else{
+
+    console.log("no")
+  }
