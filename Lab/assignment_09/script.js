2acf165b58e5602d5101e0be4f789c3a6b8d7d16
Sam <simos177@newschool.edu>
2019-05-08 23:44:38 -0400

    update

--- Lab/assignment_09/script.js
+++ Lab/assignment_09/script.js
@@ -1,49 +1,49 @@
 
-var myArray = ["red", "yellow", "green", "blue", "black", "white","orange", "purple", "pink"];
-var myOtherArray = ["car", "cat", "dog", "bird", "bike"];
+// var myArray = ["red", "yellow", "green", "blue", "black", "white","orange", "purple", "pink"];
+// var myOtherArray = ["car", "cat", "dog", "bird", "bike"];
 
 
-// myArray.push("strawberries"); //adding new values to our array
-// console.log(myArray[0]); //getting the first value of our array 
+// // myArray.push("strawberries"); //adding new values to our array
+// // console.log(myArray[0]); //getting the first value of our array 
 
-var myContainer1 = document.getElementById("container1");
-var myContainer2 = document.getElementById("container2");
-var theBody = document.body;
-var counter = 0;
+// var myContainer1 = document.getElementById("container1");
+// var myContainer2 = document.getElementById("container2");
+// var theBody = document.body;
+// var counter = 0;
 
-// while loop
+// // while loop
 
-document.getElementById("container1").innerHTML = myArray[counter];
+// document.getElementById("container1").innerHTML = myArray[counter];
 
-var myFunction = function(){
+// var myFunction = function(){
 
-console.log(counter)
+// console.log(counter)
 
-while(counter < myArray.length){
-	// do something with our array
+// while(counter < myArray.length){
+// 	// do something with our array
 
-	counter ++; //is the same as counter = counter + 1;
+// 	counter ++; //is the same as counter = counter + 1;
 
-document.getElementById("container1").innerHTML = myArray[counter];
-}
+// document.getElementById("container1").innerHTML = myArray[counter];
+// }
 
-}
+// }
 
-document.getElementById("container2").innerHTML = myOtherArray[counter];
+// document.getElementById("container2").innerHTML = myOtherArray[counter];
 
-var myFunction2 = function(){
+// var myFunction2 = function(){
 
-console.log(counter)
+// console.log(counter)
 
-while(counter < myOtherArray.length){
-	// do something with our array
+// while(counter < myOtherArray.length){
+// 	// do something with our array
 
-	counter ++; //is the same as counter = counter + 1;
+// 	counter ++; //is the same as counter = counter + 1;
 
-document.getElementById("container2").innerHTML = myOtherArray[counter];
-}
+// document.getElementById("container2").innerHTML = myOtherArray[counter];
+// }
 
-}
+// }
 
 
 
---
+++ Studio/03_Portfolio/script.js
@@ -0,0 +1,195 @@
+$(document).ready(function(){
+
+	$(".assignment1").mouseenter(function(){
+		$(".text1").css("display" , "inline-block")
+	})
+
+	$(".assignment1").mouseleave(function(){
+		$(".text1").css("display" , "none")
+	})
+
+
+	$(".assignment2").mouseenter(function(){
+		$(".text2").css("display" , "inline-block")
+	})
+
+	$(".assignment2").mouseleave(function(){
+		$(".text2").css("display" , "none")
+	})
+
+
+
+	$(".assignment3").mouseenter(function(){
+		$(".text3").css("display" , "inline-block")
+	})
+
+	$(".assignment3").mouseleave(function(){
+		$(".text3").css("display" , "none")
+	})
+
+
+
+	$(".assignment4").mouseenter(function(){
+		$(".text4").css("display" , "inline-block")
+	})
+
+	$(".assignment4").mouseleave(function(){
+		$(".text4").css("display" , "none")
+	})
+
+
+
+
+	$(".assignment5").mouseenter(function(){
+		$(".text5").css("display" , "inline-block")
+	})
+
+	$(".assignment5").mouseleave(function(){
+		$(".text5").css("display" , "none")
+	})
+
+
+
+	$(".assignment6").mouseenter(function(){
+		$(".text6").css("display" , "inline-block")
+	})
+
+	$(".assignment6").mouseleave(function(){
+		$(".text6").css("display" , "none")
+	})
+
+
+
+	$(".assignment7").mouseenter(function(){
+		$(".text7").css("display" , "inline-block")
+	})
+
+	$(".assignment7").mouseleave(function(){
+		$(".text7").css("display" , "none")
+	})
+
+
+
+
+	$(".assignment8").mouseenter(function(){
+		$(".text8").css("display" , "inline-block")
+	})
+
+	$(".assignment8").mouseleave(function(){
+		$(".text8").css("display" , "none")
+	})
+
+
+
+	$(".assignment9").mouseenter(function(){
+		$(".text9").css("display" , "inline-block")
+	})
+
+	$(".assignment9").mouseleave(function(){
+		$(".text9").css("display" , "none")
+	})
+
+
+
+
+	$(".assignment10").mouseenter(function(){
+		$(".text10").css("display" , "inline-block")
+	})
+
+	$(".assignment10").mouseleave(function(){
+		$(".text10").css("display" , "none")
+	})
+
+
+
+	$(".assignment11").mouseenter(function(){
+		$(".text11").css("display" , "inline-block")
+	})
+
+	$(".assignment11").mouseleave(function(){
+		$(".text11").css("display" , "none")
+	})
+
+
+
+	$(".assignment12").mouseenter(function(){
+		$(".text12").css("display" , "inline-block")
+	})
+
+	$(".assignment12").mouseleave(function(){
+		$(".text12").css("display" , "none")
+	})
+
+
+
+
+	$(".assignment13").mouseenter(function(){
+		$(".text13").css("display" , "inline-block")
+	})
+
+	$(".assignment13").mouseleave(function(){
+		$(".text13").css("display" , "none")
+	})
+
+
+
+	$(".assignment14").mouseenter(function(){
+		$(".text14").css("display" , "inline-block")
+	})
+
+	$(".assignment14").mouseleave(function(){
+		$(".text14").css("display" , "none")
+	})
+
+
+
+	$(".name").mouseenter(function(){
+		$(".nametext").css("display" , "inline-block")
+	})
+
+	$(".name").mouseleave(function(){
+		$(".nametext").css("display" , "none")
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+});
